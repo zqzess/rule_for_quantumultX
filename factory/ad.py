32b38bc18a36666808324e8f51cb7f673cb0a508
zqzess <zqzess@126.com>
2020-10-24 17:34:05 +0800

    quanX去广告规则

---
+++ factory/ad.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# ‐*‐ coding:utf‐8 ‐*‐
+"""code_info
+@Time : 2020 2020/10/23 15:32
+@Author : zqzess
+@File : ad.py.py
+"""
+
+# -*- coding: utf-8 -*-
+
+#
+# 提取广告规则，并且只提取对全域禁止的那种规则
+#
+
+# 参考 ADB 广告规则格式：https://adblockplus.org/filters
+import os
+import time
+import sys
+import requests
+import re
+
+from factory import ad_quanX
+
+rules_url = [
+    # EasyList China
+    #'https://easylist-downloads.adblockplus.org/easylistchina.txt',
+    # EasyList + China
+    'https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt',
+    # 乘风 去视频广告
+    'https://gitee.com/xinggsf/Adblock-Rule/raw/master/mv.txt',
+    #乘风 去广告
+    'https://gitee.com/xinggsf/Adblock-Rule/raw/master/rule.txt',
+    #大圣净化
+    'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts',
+    #anti-ad-easylist
+    'https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-easylist.txt'
+]
+
+rule = ''
+
+# contain both domains and ips
+domains = []
+
+
+for rule_url in rules_url:
+    print('loading... ' + rule_url)
+
+    # get rule text
+    success = False
+    try_times = 0
+    r = None
+    while try_times < 5 and not success:
+        r = requests.get(rule_url)
+        if r.status_code != 200:
+            time.sleep(1)
+            try_times = try_times + 1
+        else:
+            success = True
+            break
+
+    if not success:
+        sys.exit('error in request %s\n\treturn code: %d' % (rule_url, r.status_code) )
+
+    rule = rule + r.text + '\n'
+
+
+# parse rule
+rule = rule.split('\n')
+for row in rule:
+    row = row.strip()
+    row0 = row
+
+    # 处理广告例外规则
+
+    if row.startswith('@@'):
+        i = 0
+        while i < len(domains):
+            domain = domains[i]
+            if domain in row:
+                del domains[i]
+            else:
+                i = i + 1
+
+        continue
+
+
+    # 处理广告黑名单规则
+
+    # 直接跳过
+    if row=='' or row.startswith('!') or "$" in row or "##" in row:
+        continue
+
+    # 清除前缀
+    row = re.sub(r'^\|?https?://', '', row)
+    row = re.sub(r'^\|\|', '', row)
+    row = row.lstrip('.*')
+
+    # 清除后缀
+    row = row.rstrip('/^*')
+    row = re.sub(r':\d{2,5}$', '', row)  # 清除端口
+
+    # 不能含有的字符
+    if re.search(r'[/^:*]', row):
+        print('ignore: '+row0)
+        continue
+
+    # 只匹配域名或 IP
+    if re.match(r'^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,9}$', row) or re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', row):
+        domains.append(row)
+
+print('done.')
+
+
+# write into files
+if not os.path.exists('../rtmp'):
+    os.makedirs('../rtmp')
+file_ad = sys.stdout
+try:
+    if sys.version_info.major == 3:
+        file_ad = open('../rtmp/ad.list', 'w+', encoding='utf-8')
+    else:
+        file_ad = open('../rtmp/ad.list', 'w+')
+except:
+    pass
+
+file_ad.write('# adblock rules refresh time: ' + time.strftime("%Y-%m-%d %H:%M:%S") + '\n')
+
+domains = list( set(domains) )
+domains.sort()
+
+for item in domains:
+    file_ad.write(item + '\n')
+
+print("转换开始")
+ad_quanX.Change()
+print("工作结束")
---
+++ factory/ad_quanX.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# ‐*‐ coding:utf‐8 ‐*‐
+"""code_info
+@Time : 2020 2020/10/23 16:06
+@Author : zqzess
+@File : ad_quanX.py
+"""
+import os, re
+import sys
+
+
+def Change():
+    if not os.path.exists('../rules'):
+        os.makedirs('../rules')
+    out_fname = '../rules/AdBlock.list'
+    in_fname = '../rtmp/ad.list'
+    if os.path.exists(out_fname):
+        os.remove(out_fname)
+        print("文件存在，已执行删除")
+
+    file_ad = sys.stdout
+    try:
+        if sys.version_info.major == 3:
+            f1 = open(in_fname, "r", encoding="utf-8")
+            f2 = open(out_fname, "w+", encoding="utf-8")
+        else:
+            f1 = open(in_fname, "r")
+            f2 = open(out_fname, "w+")
+    except:
+        print("Error: 没有找到文件或读取文件失败")
+    else:
+        for lineTmp in f1.readlines():
+            if lineTmp.find('# adblock rules refresh time') == 0:
+                f2.write(lineTmp)
+                f2.write("\n")
+                print("注释:"+lineTmp)
+                continue
+            keywords = lineTmp
+            # pattern = re.compile(r'[a-zA-z]+')
+            result = re.search(r'[a-zA-z]+', keywords)
+            result2 = re.findall(r'\.',keywords)
+            # print(keywords)
+            # print(result2)
+            # print(result2.__len__())
+            if(result== None):
+                # print(keywords)
+                keywords=keywords.replace("\n","").replace(" ","")
+                keywords = "IP-CIDR,"+keywords+",AdBlock"
+                f2.write(keywords+"\n")
+                continue
+            if(result2.__len__()==1):
+                keywords = keywords.replace("\n", "").replace(" ", "")
+                keywords = "HOST-SUFFIX," + keywords + ",AdBlock"
+                f2.write(keywords + "\n")
+            elif(result2.__len__()==2 | result2.__len__()==3):
+                keywords = keywords.replace("\n", "").replace(" ", "")
+                keywords = "HOST," + keywords + ",AdBlock"
+                f2.write(keywords + "\n")
+        f1.close()
+        f2.close()
+
+if __name__ == '__main__':
+    Change()
