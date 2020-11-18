ce33ed56e8b180d3869b7dcf617eafc2d0e62b31
zqzess <zqzess@126.com>
2020-11-18 13:48:30 +0800

    去除大陆白名单大量重复域名

--- factory/Mainland.py
+++ factory/Mainland.py
@@ -14,7 +14,7 @@ import re
 in_fname='./rtmp/Mainland.list'
 in_fname2='./rtmp/China.list'
 in_fname3='./rtmp/ChinaIPs.list'
-in_fname4='./rtmp/ChinaTest.list'
+# in_fname4='./rtmp/ChinaTest.list'
 out_fname='../rules/Mainland.list'
 out_fname2='./rtmp/MainlandTmp.list'
 
@@ -22,7 +22,7 @@ def download():
     DATA_URL='https://raw.githubusercontent.com/GeQ1an/Rules/master/QuantumultX/Filter/Mainland.list'
     DATA_URL2='https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/China/China.list'
     DATA_URL3='https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/ChinaIPs/ChinaIPs.list'
-    DATA_URL4='https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/ChinaTest/ChinaTest.list'
+    # DATA_URL4='https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/ChinaTest/ChinaTest.list'
     if os.path.exists(in_fname):
         os.remove(in_fname)
         print("CMedia.list临时文件存在，已执行删除")
@@ -32,9 +32,9 @@ def download():
     if os.path.exists(in_fname3):
         os.remove(in_fname3)
         print("ChinaIPs.list文件存在，已执行删除")
-    if os.path.exists(in_fname4):
-        os.remove(in_fname4)
-        print("ChinaTest.list文件存在，已执行删除")
+    # if os.path.exists(in_fname4):
+    #     os.remove(in_fname4)
+    #     print("ChinaTest.list文件存在，已执行删除")
     if os.path.exists(out_fname):
         os.remove(out_fname)
         print("Mainland.list文件存在，已执行删除")
@@ -45,7 +45,7 @@ def download():
     wget.download(DATA_URL, out=in_fname)
     wget.download(DATA_URL2, out=in_fname2)
     wget.download(DATA_URL3, out=in_fname3)
-    wget.download(DATA_URL4, out=in_fname4)
+    # wget.download(DATA_URL4, out=in_fname4)
 
 def Mainland():
     print("Mainland")
@@ -120,38 +120,38 @@ def ChinaIPs():
                 continue
         f1.close()
         f2.close()
-def ChinaTest():
-    print("ChinaTest")
-    try:
-        if sys.version_info.major == 3:
-            f1 = open(in_fname4, "r", encoding="utf-8")
-            f2 = open(out_fname2, "a+", encoding="utf-8")
-        else:
-            f1 = open(in_fname4, "r")
-            f2 = open(out_fname2, "a+")
-    except:
-        print("Error: 没有找到文件或读取文件失败")
-    else:
-        f2.write("\n")
-        for lineTmp in f1.readlines():
-            if lineTmp.find('#') == 0:
-                print("注释:" + lineTmp)
-                continue
-            keywords = lineTmp
-            result = re.search('ChinaTest', keywords)
-            if result != None:
-                keywords = keywords.replace("ChinaTest", "Mainland")
-                f2.write(keywords)
-                continue
-        f1.close()
-        f2.close()
+# def ChinaTest():
+#     print("ChinaTest")
+#     try:
+#         if sys.version_info.major == 3:
+#             f1 = open(in_fname4, "r", encoding="utf-8")
+#             f2 = open(out_fname2, "a+", encoding="utf-8")
+#         else:
+#             f1 = open(in_fname4, "r")
+#             f2 = open(out_fname2, "a+")
+#     except:
+#         print("Error: 没有找到文件或读取文件失败")
+#     else:
+#         f2.write("\n")
+#         for lineTmp in f1.readlines():
+#             if lineTmp.find('#') == 0:
+#                 print("注释:" + lineTmp)
+#                 continue
+#             keywords = lineTmp
+#             result = re.search('ChinaTest', keywords)
+#             if result != None:
+#                 keywords = keywords.replace("ChinaTest", "Mainland")
+#                 f2.write(keywords)
+#                 continue
+#         f1.close()
+#         f2.close()
 
 def mainchange():
     download()
     Mainland()
     China()
     ChinaIPs()
-    ChinaTest()
+    # ChinaTest()
 #去重
     a = 0
     lines_seen = set()
