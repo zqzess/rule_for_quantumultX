64006c531f830f51a355382e7c26810959ae79bc
zqzess <zqzess@126.com>
2023-07-29 22:14:45 +0800

    通知微调

--- js/Mine/wnCalendar/wnCalendar.js
+++ js/Mine/wnCalendar/wnCalendar.js
@@ -54,7 +54,7 @@ function doWork(){
                     desc += i.term?' ' + i.term:''
                     desc += i.value?' ' + i.value:''
                     // 拼接消息体
-                    notifyContent = '干支纪法：' + i.gzYear + '年 ' + i.gzMonth + '月 ' + i.gzDate + '日\n📆农历：' + i.lMonth + '月' + i.lDate + '\n🎉今日：' + desc + '\n🈲️忌：' + i.avoid + '\n✅宜：' + i.suit
+                    notifyContent = '干支纪法：' + i.gzYear + '年 ' + i.gzMonth + '月 ' + i.gzDate + '日' + desc + '\n🈲️忌：' + i.avoid + '\n✅宜：' + i.suit
                 }
             })
             $.isSurge() ? body = {
